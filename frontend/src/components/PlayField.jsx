6fd0377d9c06516c2da601e2774e25db89be02da
Nirajan <nirajanacharya787@gmail.com>
2026-01-06 17:33:28 +0545

    Use Playfield flexbox part 2

---
+++ frontend/src/components/PlayField.jsx
@@ -0,0 +1,8 @@
+import ChessBoard from "./chessboard.jsx";
+import "../css/PlayField.css"
+const PlayField = () => {
+    return <div className="Play-Field">
+        <ChessBoard></ChessBoard>
+    </div>
+}
+export default PlayField;
